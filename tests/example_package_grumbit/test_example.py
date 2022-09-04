ad8c25a5774c5395399500a8a42b600fadfc4967
Graham Coster <graham.coster@conjura.com>
2022-09-04 20:06:13 +1000

    check fails on test failure

--- tests/example_package_grumbit/test_example.py
+++ tests/example_package_grumbit/test_example.py
@@ -5,4 +5,4 @@ from src.example_package_grumbit.example import add_one
 
 class TestExample(TestCase):
     def test_add_one(self):
-        self.assertEqual(add_one(2), 3)
+        self.assertEqual(add_one(2), 2)
