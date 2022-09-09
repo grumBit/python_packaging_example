fa0a26af583238ba72f11016d150c272965cdf61
Graham Coster <graham.coster@conjura.com>
2022-09-09 18:15:15 +1000

    fix test type check

--- src/example_package_grumbit/example.py
+++ src/example_package_grumbit/example.py
@@ -1,6 +1,6 @@
-def add_one(number: int) -> str:
+def add_one(number: int) -> int:
     return number + 1
 
 
-def add_two(number: int) -> str:
+def add_two(number: int) -> int:
     return number + 2
